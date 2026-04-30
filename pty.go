baeb6b9d2715b9ada7bb7442ce764e6a3f9403c0
phuslu <phus.lu@gmail.com>
2026-04-30 17:33:07 +0800

    tweak pty types

--- pty.go
+++ pty.go
@@ -1,6 +1,9 @@
 package pty
 
-import "io"
+import (
+	"io"
+	"os"
+)
 
 // Pty is the master side of a pseudo terminal.
 type Pty interface {
@@ -11,8 +14,10 @@ type Pty interface {
 
 // Winsize describes a terminal window size.
 type Winsize struct {
-	Rows uint16
-	Cols uint16
-	X    uint16
-	Y    uint16
+	Rows uint16 // ws_row: Number of rows (in cells).
+	Cols uint16 // ws_col: Number of columns (in cells).
+	X    uint16 // ws_xpixel: Width in pixels.
+	Y    uint16 // ws_ypixel: Height in pixels.
 }
+
+var _ Pty = (*os.File)(nil)
